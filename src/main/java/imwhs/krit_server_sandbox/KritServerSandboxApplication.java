cdde487fe7d5e1104cae7166cf75f1206419ec1a
imWhS <heextory@icloud.com>
2026-09-08 15:40:52 +0900

    chore: Initialize project

---
+++ src/main/java/imwhs/krit_server_sandbox/KritServerSandboxApplication.java
@@ -0,0 +1,13 @@
+package imwhs.krit_server_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KritServerSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KritServerSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/imwhs/krit_server_sandbox/KritServerSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package imwhs.krit_server_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KritServerSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
