f74f88481bbb5e86f1a7c0f3a03f225bd673abeb
imWhS <heextory@icloud.com>
2026-09-09 18:36:51 +0900

    feat: 게시물(Post) 본문의 데이터 타입을 TEXT로 변경

--- src/main/java/imwhs/krit_server_sandbox/domain/Post.java
+++ src/main/java/imwhs/krit_server_sandbox/domain/Post.java
@@ -1,5 +1,6 @@
 package imwhs.krit_server_sandbox.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,8 +13,19 @@ public class Post {
     @Id @GeneratedValue
     private Long id;
 
+    /**
+     * 제목
+     */
+    @Column(length = 100, nullable = false)
     private String title;
 
+    /**
+     * 본문
+     * <ul>
+     *     <li> 대용량 텍스트를 저장하기 위해 VARCHAR 대신 TEXT 타입을 사용합니다. </li>
+     * </ul>
+     */
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createdAt;
