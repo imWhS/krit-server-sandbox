9139940b8f59ed23bfedd9a3b87bcd7c98d5f298
imWhS <heextory@icloud.com>
2026-09-10 18:30:58 +0900

    fix: AuditorAwareImpl에서 작업자 정보를 사용자 ID(Account의 PK)로 제공하도록 수정

--- src/main/java/imwhs/krit_server_sandbox/KritServerSandboxApplication.java
+++ src/main/java/imwhs/krit_server_sandbox/KritServerSandboxApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl") // 작업자 정보를 제공받을 빈 설정 후 JPA Auditing 활성화
 @SpringBootApplication
 public class KritServerSandboxApplication {
 
--- src/main/java/imwhs/krit_server_sandbox/domain/AuditorAwareImpl.java
+++ src/main/java/imwhs/krit_server_sandbox/domain/AuditorAwareImpl.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+/**
+ * JPA Auditing에게 현재 작업자 정보를 제공합니다.
+ */
 @Component
-public class AuditorAwareImpl implements AuditorAware<String> {
+public class AuditorAwareImpl implements AuditorAware<Long> {
 
     @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.of("admin"); // TODO: Spring Security 인증 구현 후 SecurityContextHolder 통해 세션의 사용자 정보로 설정
+    public Optional<Long> getCurrentAuditor() {
+        return Optional.of(1L); // TODO: Spring Security 인증 구현 후 SecurityContextHolder 통해 세션의 사용자 정보로 설정
     }
 
 }
