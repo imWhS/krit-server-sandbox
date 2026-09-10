48e83723e76767653db051026001a220ce5ba197
imWhS <heextory@icloud.com>
2026-09-10 17:18:38 +0900

    feat: 엔티티 별 요청자/작업자 메타데이터 자동 기록을 위한 AuditorAware 구현 및 등록

---
+++ src/main/java/imwhs/krit_server_sandbox/domain/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package imwhs.krit_server_sandbox.domain;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("admin");
+    }
+
+}
