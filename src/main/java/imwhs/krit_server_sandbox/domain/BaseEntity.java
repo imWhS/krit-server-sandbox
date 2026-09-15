8a73b2d5c886207150d5f9074aa42d4810b0bba5
imWhS <heextory@icloud.com>
2026-09-15 20:26:09 +0900

    fix: BaseTimeEntity, BaseEntity에서 누락됐던 @Getter 추가

--- src/main/java/imwhs/krit_server_sandbox/domain/BaseEntity.java
+++ src/main/java/imwhs/krit_server_sandbox/domain/BaseEntity.java
@@ -3,10 +3,12 @@ package imwhs.krit_server_sandbox.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity extends BaseTimeEntity {
