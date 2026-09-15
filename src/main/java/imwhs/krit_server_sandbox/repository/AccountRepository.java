70472041b716321814c52262fbdb55b760a316ba
imWhS <heextory@icloud.com>
2026-09-15 21:32:51 +0900

    feat: 계정의 공개 식별자/이메일 주소로 Account 엔티티 조회/존재 여부 확인

--- src/main/java/imwhs/krit_server_sandbox/repository/AccountRepository.java
+++ src/main/java/imwhs/krit_server_sandbox/repository/AccountRepository.java
@@ -2,9 +2,15 @@ package imwhs.krit_server_sandbox.repository;
 
 import imwhs.krit_server_sandbox.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByHandleAndDeletedAtIsNull(String handle);
+
+    boolean existsByHandleAndDeletedAtIsNull(String handle);
+
+    boolean existsByEmailAndDeletedAtIsNull(String email);
 
 }
