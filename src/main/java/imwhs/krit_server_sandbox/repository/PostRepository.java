6695486513a91427a3cb19f3a628aad9a90354c7
imWhS <heextory@icloud.com>
2026-09-16 00:18:57 +0900

    feat: 게시물(Post)의 필수적인 등록, 조회, 업데이트, 논리적 삭제 서비스 구현

--- src/main/java/imwhs/krit_server_sandbox/repository/PostRepository.java
+++ src/main/java/imwhs/krit_server_sandbox/repository/PostRepository.java
@@ -3,6 +3,10 @@ package imwhs.krit_server_sandbox.repository;
 import imwhs.krit_server_sandbox.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findByIdAndDeletedAtIsNull(Long id);
 
 }
