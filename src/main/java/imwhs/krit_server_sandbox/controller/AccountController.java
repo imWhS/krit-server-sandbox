20b2147f3946ad1cf5dd3b21d224bd2b6e67dc1f
imWhS <heextory@icloud.com>
2026-09-16 01:47:10 +0900

    feat: 계정 논리적 삭제 API 엔드포인트

--- src/main/java/imwhs/krit_server_sandbox/controller/AccountController.java
+++ src/main/java/imwhs/krit_server_sandbox/controller/AccountController.java
@@ -7,13 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v0/account")
+@RequestMapping("/api/v0/accounts")
 @RequiredArgsConstructor
 @RestController
 public class AccountController {
 
     private final AccountService accountService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public AccountResponse registerAccount(@RequestBody AccountRegisterRequest request) {
         Account account = accountService.register(request.getHandle(), request.getEmail(), request.getPassword());
@@ -32,6 +33,7 @@ public class AccountController {
         return AccountResponse.from(account);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}/password")
     public void updateAccountPassword(
             @PathVariable Long id,
@@ -53,6 +55,14 @@ public class AccountController {
             @PathVariable Long id,
             @RequestBody AccountImageUrlUpdateRequest request) {
         accountService.updateImageUrl(id, request.imageUrl());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void softDeleteAccount(
+            @PathVariable Long id,
+            @RequestParam Long actorId) { // TODO: 스프링 시큐리티 도입 후 세션 인증 정보에서 가져오도록 변경
+        accountService.softDelete(id, actorId);
     }
 
 }
