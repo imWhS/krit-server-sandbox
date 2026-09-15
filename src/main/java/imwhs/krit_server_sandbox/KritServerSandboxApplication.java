9e0bca001858d6a101ce86b9e24f03e5303c918a
imWhS <heextory@icloud.com>
2026-09-15 20:38:53 +0900

    fix: @EnableJpaAuditing의 auditorAwareRef 속성 제거 후 AuditorAware를 타입 기반으로 자동 주입받도록 수정

--- src/main/java/imwhs/krit_server_sandbox/KritServerSandboxApplication.java
+++ src/main/java/imwhs/krit_server_sandbox/KritServerSandboxApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl") // 작업자 정보를 제공받을 빈 설정 후 JPA Auditing 활성화
+@EnableJpaAuditing // 작업자 정보를 제공받을 빈 설정 후 JPA Auditing 활성화 (AuditorAware가 유일해서 타입 기반으로 자동 주입 설정)
 @SpringBootApplication
 public class KritServerSandboxApplication {
 
